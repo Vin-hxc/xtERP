f84d3f0275c0382d6269e3c8f2de084fe1431f19
q2968203950 <2968203950@qq.com>
2019-12-16 10:53:50 +0800

    采购模块实体类

--- src/main/java/com/xt/mapper/hjn/OrderMapper.java
+++ src/main/java/com/xt/mapper/hjn/OrderMapper.java
@@ -9,6 +9,7 @@ import org.apache.ibatis.annotations.Update;
 import org.springframework.core.annotation.Order;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -38,21 +39,6 @@ public interface OrderMapper {
     //@Insert("insert into order values(null,#{orderid},#{supplierid},#{purchaserid},#{rebate},#{paytype},#{total},#{amount_paid},#{total_tax},#{orderstate},#{preferential},#{costid})")
     int addOrder(Order order);
 
-    /**
-     * 新增采购明细
-     *
-     * @return
-     */
-    //@Insert("insert into Detailed values(null,#{orderid},#{storehouseid},#{goodsid},#{number},#{tax},#{price},#{remarks}")
-    int addDetailed(Detailed detailed);
-
-    /**
-     * 查询采购订单的明细顶到怕
-     *
-     * @return
-     */
-    @Select("select * from Detailed  where orderid = #{orderid}")
-    List<Detailed> queryDetailed(int orderid);
 
     /**
      * 审核订单
@@ -79,36 +65,23 @@ public interface OrderMapper {
     int updateOrder(int id,int supplierid,int purchaserid,int rebate,int amount_paid);
 
     /**
-     * 添加采购支出
-     *
+     * 查询所有审核状态为 2 的订单费用
      * @return
      */
-    //@Insert("(insert into cost values(null,#{orderid},#{costtype},#{costprice}))")
-    int addCost(Cost cost);
-
-
-    /**
-     * 删除采购支出
-     *
-     * @return
-     */
-    //@Delete("(delect from cost where id=#{id} )")
-    int delectCost(int id);
+    @Select("select o.orderid as id,sl.supplier as sName,sl.accountNumber as accoun," +
+            " o.purchaserid as pid,o.total as total,o.amount_paid as amount,ct.costtype as costType,ct.costprice as costPrice " +
+            " from orders o LEFT JOIN cost ct ON o.costid=ct.id " +
+            " LEFT JOIN supplier sl on o.supplierid=sl.id" +
+            " where o.orderstate=2 and sl.type='供应商' and sl.delete_Flag=0 and o.finance=0 ")
+    List<HashMap> incurExpense();
 
     /**
-     * 查采购支出
-     *
+     * 修改财务记录添加状态
+     * @param id
      * @return
      */
-   // @Select("(delect from cost where orderid=#{orderid} )")
-    List<Cost> findCost(int orderid);
+    @Update("update orders set finance=1 where orderid=#{id}")
+    boolean updateFinance(Integer id);
 
-    /**
-     * 修改采购支出
-     *
-     * @return
-     */
-    //@Update("(update  cost set costprice=#{costprice},costtype=#{costtype}  where id=#{id} )")
-    int delectCost(int id,String costtype,int costprice);
 
 }
