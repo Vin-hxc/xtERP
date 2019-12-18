f3bbfe1820fd93facef17df8dec28ad321768428
Vin-hxc <779065985@qq.com>
2019-12-18 13:47:09 +0800

    Vin
    Signed-off-by: Vin-hxc <779065985@qq.com>

---
+++ src/main/java/com/xt/service/vin/ProductModelsServiceI.java
@@ -0,0 +1,46 @@
+package com.xt.service.vin;
+
+import com.xt.entity.vin.Product_model;
+import com.xt.entity.vin.Product_type;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface ProductModelsServiceI {
+    /**
+     * 查询所有产品型号
+     * @return
+     */
+    public List<HashMap> getAllProductModels();
+
+    /**
+     * 新增产品型号
+     * @param product_model
+     * @return
+     */
+    public boolean insertProductModels(Product_model product_model);
+
+    /**
+     * 修改产品型号信息
+     * @param product_model
+     * @return
+     */
+    public boolean updateProductModels(Product_model product_model);
+
+    /**
+     * 修改产品型号删除标记
+     * @param id
+     * @return
+     */
+    public boolean deleteProductModels(long id);
+
+    /**
+     * 获取产品类别
+     * @return
+     */
+    public List<Product_type> queryType();
+
+}
---
+++ src/main/java/com/xt/service/vin/impl/ProductModelsServiceImpl.java
@@ -0,0 +1,44 @@
+package com.xt.service.vin.impl;
+
+import com.xt.entity.vin.Product_model;
+import com.xt.entity.vin.Product_type;
+import com.xt.mapper.vin.ProductModelsMapper;
+import com.xt.service.vin.ProductModelsServiceI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+@SuppressWarnings("all")
+public class ProductModelsServiceImpl implements ProductModelsServiceI {
+
+    @Autowired
+    private ProductModelsMapper productModelsMapper;
+
+    @Override
+    public List<HashMap> getAllProductModels() {
+        return productModelsMapper.getAllProductModels();
+    }
+
+    @Override
+    public boolean insertProductModels(Product_model product_model) {
+        return productModelsMapper.insertProductModels(product_model);
+    }
+
+    @Override
+    public boolean updateProductModels(Product_model product_model) {
+        return productModelsMapper.updateProductModels(product_model);
+    }
+
+    @Override
+    public boolean deleteProductModels(long id) {
+        return productModelsMapper.deleteProductModels(id);
+    }
+
+    @Override
+    public List<Product_type> queryType() {
+        return productModelsMapper.queryType();
+    }
+}
